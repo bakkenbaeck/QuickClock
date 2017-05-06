e66acb2433b3e02408008dcb5ed091246780b0bb
yuliaveres <juliaveresklia@gmail.com>
2017-05-06 19:16:00 +0200

    Connect scenario and UI; add time string

--- iOS/Source/Items/Controllers/MessagesViewController.swift
+++ iOS/Source/Items/Controllers/MessagesViewController.swift
@@ -17,6 +17,12 @@ struct IndexPathSizes {
 
 class MessagesViewController: UIViewController {
     
+    lazy var scenario: Scenario = {
+        let scenario = Scenario()
+        scenario.delegate = self
+        
+        return scenario
+    }()
     
     var sizeFor = IndexPathSizes()
     
@@ -53,6 +59,7 @@ class MessagesViewController: UIViewController {
         return view
     }()
 
+    static let animationViewHeightConstraint: CGFloat = 50.0
     
     lazy var animationView: UIView = {
         let view = AnimationContainerView(frame: CGRect.zero)
@@ -61,25 +68,27 @@ class MessagesViewController: UIViewController {
     }()
 
     var textInputBottomConstraint: NSLayoutConstraint!
-    var animationContainerHeightConsraint: NSLayoutConstraint!
+    var animationContainerHeightConsraint: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "@Quick Clock"
         
+        view.backgroundColor = .white
+        
         view.addSubview(collectionView)
         view.addSubview(textInputView)
         view.addSubview(animationView)
         
-        textInputView.left(to: view)
-        textInputView.right(to: view)
-        textInputBottomConstraint = textInputView.bottom(to: view)
+        textInputView.left(to: view, offset: 3.0)
+        textInputView.right(to: view, offset: 3.0)
+        textInputBottomConstraint = textInputView.bottom(to: view, offset: -5.0)
         textInputView.height(40.0)
         
         animationView.left(to: view)
         animationView.right(to: view)
-        animationView.height(50.0)
+        animationContainerHeightConsraint = animationView.height(0.0)
         animationView.bottomToTop(of: textInputView)
         
         collectionView.top(to: view)
@@ -95,6 +104,40 @@ class MessagesViewController: UIViewController {
         
         return dummyCell.size(for: collectionView.bounds.width)
     }
+    
+    func stopTypingAnimation() {
+        self.animationContainerHeightConsraint?.constant = 0.0
+        self.view.layoutIfNeeded()
+    }
+    
+    func startTypingAnimation() {
+        self.animationContainerHeightConsraint?.constant = MessagesViewController.animationViewHeightConstraint
+        self.view.setNeedsLayout()
+        self.view.layoutIfNeeded()
+    }
+}
+
+extension MessagesViewController: ScenarioDelegate {
+    func didResponse(on scenario: Scenario) {
+        self.messages.append(Message(title: "Clocky", text: "No idea", didSent: false, image: nil))
+        collectionView.reloadData()
+    }
+    
+    func didPause(on scenario: Scenario) {
+        self.stopTypingAnimation()
+    }
+    
+    func didType(on scenario: Scenario) {
+        self.startTypingAnimation()
+    }
+    
+    func didReadMessage(on scenario: Scenario) {
+    
+    }
+    
+    func didDeliverMessage(on scenario: Scenario) {
+    
+    }
 }
 
 extension MessagesViewController: MessageInputViewDelegate {
@@ -103,6 +146,14 @@ extension MessagesViewController: MessageInputViewDelegate {
         if let text = inputView.textField.text as String? {
             self.messages.append(Message(title: "Y.O.U.", text: text, didSent: true, image: nil))
             collectionView.reloadData()
+            
+            self.scenario.createScenario()
+            self.scenario.executeScenario()
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.startTypingAnimation()
+            })
+            
             collectionView.contentOffset = CGPoint(x: 0.0, y: self.collectionView.contentSize.height - 15.0)
         }
     }
