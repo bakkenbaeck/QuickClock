fc06773dea718c8e42e791fe3b02f7711247ac9c
yuliaveres <juliaveresklia@gmail.com>
2017-05-06 17:39:33 +0200

    Remove other not needed comments

--- iOS/Source/Root/iOSDevCenters+GIF.swift
+++ iOS/Source/Root/iOSDevCenters+GIF.swift
@@ -1,15 +1,6 @@
-//
-//  iOSDevCenters+GIF.swift
-//  GIF-Swift
-//
-//  Created by iOSDevCenters on 11/12/15.
-//  Copyright © 2016 iOSDevCenters. All rights reserved.
-//
-
 import UIKit
 import ImageIO
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
+
 fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
   switch (lhs, rhs) {
   case let (l?, r?):
